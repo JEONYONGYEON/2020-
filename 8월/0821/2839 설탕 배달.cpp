b764d17b3ffdc583176e00040e5cbb2aeab6d7ca
savior1002@naver.com <savior1002@naver.com>
2020-08-21 18:57:44 +0900

    no message

---
+++ 8월/0821/2839 설탕 배달.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int N;
+	int cnt = 0;
+	scanf("%d", &N); 
+	
+	cnt = N / 5;
+	if (N % 5 != 0)
+	{
+		for(;cnt >= 0; cnt--)
+		{
+			if((N - 5*cnt) % 3 == 0)
+			{
+				cnt += (N - 5 * cnt) / 3;
+				break;
+			}
+		}
+	}
+	printf("%d", cnt);
+	return 0;
+ } 
